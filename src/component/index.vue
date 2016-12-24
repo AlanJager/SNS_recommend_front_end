80417f916273cac9b774bd804e0bd458588007c9
AlanJager <873863981@qq.com>
2016-12-24 11:07:10 +0800

    update author

--- src/component/index.vue
+++ src/component/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="index">
     <h1>I am a title.</h1>
-    <p> written by {{ author }} </p>
+    <p> written <a class='author' href="https://github.com/AlanJager">{{ author_1 }}</a> &  <a class='author' href="https://github.com/ywcmaike">{{ author_2 }}</a> </p>
   </div>
 </template>
 
@@ -9,11 +9,15 @@
 export default {
   data () {
     return {
-      author: "By AlanJager"
+      author_1: "By AlanJager",
+      author_2: "ywcmaike"
     }
   }
 }
 </script>
 
 <style>
+.author {
+  color: #4090e5
+}
 </style>
